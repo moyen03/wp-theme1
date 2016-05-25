aae5d9cdea86fb13290fa808bc5abc432514309b
Md Moyen Uddin <moyen.ruet@yahoo.com>
2016-05-26 01:18:42 +0200

    single post navigation design

--- single.php
+++ single.php
@@ -17,7 +17,21 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			//			the_post_navigation();
+			?>
+			<nav class="navigation post-navigation" role="navigation">
+				<div class="post-nav-box clear">
+					<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'my-simone' ); ?></h1>
+					<div class="nav-links">
+						<?php
+						previous_post_link( '<div class="nav-previous"><div class="nav-indicator">' . _x( 'Previous Post:', 'Previous post', 'my-simone' ) . '</div><h1>%link</h1></div>', '%title' );
+						next_post_link(     '<div class="nav-next"><div class="nav-indicator">' . _x( 'Next Post:', 'Next post', 'my-simone' ) . '</div><h1>%link</h1></div>', '%title' );
+						?>
+					</div><!-- .nav-links -->
+				</div><!-- .post-nav-box -->
+			</nav><!-- .navigation -->
+
+				<?php
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
